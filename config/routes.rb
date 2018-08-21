271732968dd9e2199b9685e02070d8934d0d2767
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-08-21 12:39:47 -0400

    Simplify the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   namespace :jira do
     resources :users, only: %i[new create]
+    resources :projects, only: %i[index]
+    resources :issues, only: %i[new create]
   end
-  namespace :jira do
-    resources :projects
-  end
-  namespace :jira do
-    resources :issues
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
