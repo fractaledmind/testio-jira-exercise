b40b8eba19a2ca6f99f4cbacdf88eaedfa7413de
Stephen Margheim <margheim@mail.med.upenn.edu>
2018-08-21 12:41:31 -0400

    Get the third step of the UX (kinda) working :: creating a new Jira::Issue

--- app/controllers/jira/issues_controller.rb
+++ app/controllers/jira/issues_controller.rb
@@ -1,58 +1,57 @@
 class Jira::IssuesController < ApplicationController
-  before_action :set_jira_issue, only: [:show, :edit, :update, :destroy]
-
-  # GET /jira/issues
-  def index
-    @jira_issues = Jira::Issue.all
-  end
-
-  # GET /jira/issues/1
-  def show
-  end
-
-  # GET /jira/issues/new
   def new
     @jira_issue = Jira::Issue.new
   end
 
-  # GET /jira/issues/1/edit
-  def edit
-  end
-
-  # POST /jira/issues
   def create
     @jira_issue = Jira::Issue.new(jira_issue_params)
-
-    if @jira_issue.save
-      redirect_to @jira_issue, notice: 'Issue was successfully created.'
-    else
+    begin
+      jira_create_issue_via_api
+      redirect_to new_jira_issue_path,
+                  notice: 'Issue was successfully created.'
+    rescue RestClient::BadRequest => e
+      JSON.parse(e.http_body)['errors'].each do |k, m|
+        @jira_issue.errors.add(k,
+                               message: m)
+      end
       render :new
     end
   end
 
-  # PATCH/PUT /jira/issues/1
-  def update
-    if @jira_issue.update(jira_issue_params)
-      redirect_to @jira_issue, notice: 'Issue was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /jira/issues/1
-  def destroy
-    @jira_issue.destroy
-    redirect_to jira_issues_url, notice: 'Issue was successfully destroyed.'
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_jira_issue
-      @jira_issue = Jira::Issue.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def jira_issue_params
-      params.require(:jira_issue).permit(:title, :description, :jira_project_id)
-    end
+  def jira_create_issue_via_api
+    jira_response = authenticated_jira_request(
+                      method: :post,
+                      url: jira_issue_path,
+                      payload: jira_issue_payload,
+                      headers: { content_type: :json, accept: :json }
+                    )
+    JSON.parse(jira_response.body)
+  end
+
+  def jira_create_issue_response
+    RestClient::Request.execute(
+                                user: jira_user.username,
+                                password: jira_user.password)
+  end
+
+  def jira_issue_payload
+    {
+      fields: {
+        summary: jira_issue_params[:title],
+        description: jira_issue_params[:description],
+        project: { id: jira_issue_params[:jira_project_id] },
+        issuetype: { id: 10000 }
+      }
+    }.to_json
+  end
+
+  def jira_issue_path
+    jira_instance_url + 'rest/api/2/issue'
+  end
+
+  def jira_issue_params
+    params.require(:jira_issue).permit(:title, :description, :jira_project_id)
+  end
 end
